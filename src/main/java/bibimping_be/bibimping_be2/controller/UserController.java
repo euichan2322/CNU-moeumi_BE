f0899c73aa5864d2abafdbeea7d11a9c2141b11f
euichan-desktop <test>
2025-01-07 08:51:04 +0900

    jpa to data jpa, db connection error 해결

--- src/main/java/bibimping_be/bibimping_be2/controller/UserController.java
+++ src/main/java/bibimping_be/bibimping_be2/controller/UserController.java
@@ -2,18 +2,23 @@ package bibimping_be.bibimping_be2.controller;
 
 import bibimping_be.bibimping_be2.entity.User;
 import bibimping_be.bibimping_be2.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/register")
-    public User registerUser(@RequestBody User user) {
-        return userRepository.save(user);
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 }
