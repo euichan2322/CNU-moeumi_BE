529abcaa1cc042f8cf9f59d5f9f5738bc1566ba3
Eui-chan Jung <78064289+euichan2322@users.noreply.github.com>
2025-01-10 06:10:55 +0900

    회원가입 코드 request body와 response body를 User엔티티에서 SignUp DTO로 수정했습니다.

--- src/main/java/bibimping_be/bibimping_be2/entity/User.java
+++ src/main/java/bibimping_be/bibimping_be2/entity/User.java
@@ -38,41 +38,4 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-
-
-    // 기본 생성자
-    public User() {
-    }
-
-    // Private 생성자 (Builder에서만 호출 가능)
-    private User(UserBuilder builder) {
-        this.id = builder.id;
-        this.accountId = builder.accountId;
-        this.password = builder.password;
-    }
-    // Static inner Builder class
-    public static class UserBuilder {
-        private Long id;
-        private String accountId;
-        private String password;
-
-        public UserBuilder setId(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public UserBuilder setAccountId(String accountId) {
-            this.accountId = accountId;
-            return this;
-        }
-
-        public UserBuilder setPassword(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public User build() {
-            return new User(this);
-        }
-    }
 }
